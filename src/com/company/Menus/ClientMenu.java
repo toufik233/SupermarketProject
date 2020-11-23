f7222eddbd857be643a1c0e1f5a0a5752b90aea6
Samia Ouerfelli <73239375+souerfelli@users.noreply.github.com>
2020-11-23 14:11:49 +0100

    Creation of the client menu

--- src/com/company/Menus/ClientMenu.java
+++ src/com/company/Menus/ClientMenu.java
@@ -1,49 +1,48 @@
-package com.company.Main;
+package com.company.Menus;
 
-import com.company.Menus.ClientMenu;
+import com.company.Main.Main;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+
+public class ClientMenu {
 
     public static Scanner scanner = new Scanner(System.in);
     public static boolean quit = false;
 
-    public static void main(String[] args) {
+    public ClientMenu() {
+        myClientMenu();
+        myMenuLoop();
+    }
 
-
+    private static void myMenuLoop(){
         do {
-            menu();
             userEntry();
         } while (!quit);
-
-
     }
 
-
-    // Printing the options on the menu
-    public static void menu() {
+    private static void myClientMenu() {
         System.out.println("\n \n What do you want to do ? \n");
-        System.out.println("1) Log in as a client");
-        System.out.println("2) Log in as an administrator");
-        System.out.println("3) Exit");
+        System.out.println("1) List products");
+        System.out.println("2) My cart");
+        System.out.println("3) Logout");
     }
-
     private static void userEntry() {
         try {
             int userChoice = scanner.nextInt();
             // Swich case for user choice
             switch (userChoice) {
                 case 1:
-                    // object client
-                    new ClientMenu();
+                    // list products
                     break;
                 case 2:
-                    //object admin
+                    //my cart
                     break;
                 case 3:
+                    //logout - get back to the main menu
                     quit = true;
+                    Main.menu();
                     break;
                 default:
                     System.out.println("Invalid choice, please select one of the options above");
@@ -52,6 +51,8 @@ public class Main {
         } catch (InputMismatchException e) {
             System.out.println("Your user input is not correct, please try again");
             scanner.nextLine();
+
         }
+
     }
 }
